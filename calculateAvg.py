2377108061ef7b4cb3453b68c3ab0a66a9ef1d5a
ali <alikhorasani2002@gmail.com>
2022-09-28 20:11:12 +0330

    calculate total of multies

--- calculateAvg.py
+++ calculateAvg.py
@@ -1,12 +1,20 @@
+from tokenize import Double
+
+
 calAvg=lambda score,multy: score*multy
 run=True
 scoreList=[]
+multyList=[]
 total=0
+mulTotal=0
 while(run):
-    score=int(input('please enter score: '))
+    score=float(input('please enter score: '))
     multy=int(input('please enter multy: '))
     scoreList.append(calAvg(score,multy))
+    multyList.append(multy)
     answer=int(input('do you want to add another? yes(1) or no(0)'))
     if answer==0: run=False
 for i in scoreList:
     total+=i
+for i in multyList:
+    mulTotal+=i
