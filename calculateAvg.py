627403e26ec9e32082c71f953e29811791133995
ali <alikhorasani2002@gmail.com>
2022-09-28 20:03:50 +0330

    add inputs and variabls

--- calculateAvg.py
+++ calculateAvg.py
@@ -1,1 +1,9 @@
 calAvg=lambda score,multy: score*multy
+run=True
+scoreList=[]
+while(run):
+    score=int(input('please enter score: '))
+    multy=int(input('please enter multy: '))
+    scoreList.append(calAvg(score,multy))
+    answer=int(input('do you want to add another? yes(1) or no(0)'))
+    if answer==0: run=False
